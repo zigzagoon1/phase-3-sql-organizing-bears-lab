90d8e20d1dedcb025cf9ced2e84ebd39801e066a
KellyMark <kmark602@gmail.com>
2023-05-05 20:31:07 -0400

    commit tree 90562bf16d92b7523d6d718424c1ee6decbdf48c

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'Black', 'Friendly', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 8, 'F', 'Brown', 'Mean', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'M', 'Brown', 'Shy', FALSE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'Brown', 'Shy', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 6, 'F', 'White', 'Mean', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'F', 'Black', 'Friendly', FALSE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, NULL, 'M', 'Black', 'Friendly', TRUE);
+
+
+
+
+
+
+
